b0d1e06ebad393bdcc93ffa641ba560587d38ece
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2021-03-30 15:08:17 +0300

    add decoupled version of MultiLossFactory

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -1,9 +1,9 @@
 from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
-from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
+from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory, MultiLossDecoupledFactory
 from .regression_loss import SmoothL1Loss, WingLoss
 
 __all__ = [
-    'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
+    'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss', 'MultiLossDecoupledFactory',
     'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss'
 ]
--- mmpose/models/losses/multi_loss_factory.py
+++ mmpose/models/losses/multi_loss_factory.py
@@ -253,3 +253,40 @@ class MultiLossFactory(nn.Module):
                 pull_losses.append(None)
 
         return heatmaps_losses, push_losses, pull_losses
+
+
+@LOSSES.register_module()
+class MultiLossDecoupledFactory(MultiLossFactory):
+
+    def forward(self, outputs, heatmaps, masks, joints):
+        heatmaps_losses = []
+        push_losses = []
+        pull_losses = []
+        for idx in range(len(outputs)):
+            if self.heatmaps_loss[idx]:
+                heatmaps_pred = outputs[idx][0]
+                heatmaps_loss = self.heatmaps_loss[idx](heatmaps_pred,
+                                                        heatmaps[idx],
+                                                        masks[idx])
+                heatmaps_loss = heatmaps_loss * self.heatmaps_loss_factor[idx]
+                heatmaps_losses.append(heatmaps_loss)
+            else:
+                heatmaps_losses.append(None)
+
+            if self.ae_loss[idx]:
+                tags_pred = outputs[idx][1]
+                batch_size = tags_pred.size()[0]
+                tags_pred = tags_pred.contiguous().view(batch_size, -1, 1)
+
+                push_loss, pull_loss = self.ae_loss[idx](tags_pred,
+                                                         joints[idx])
+                push_loss = push_loss * self.push_loss_factor[idx]
+                pull_loss = pull_loss * self.pull_loss_factor[idx]
+
+                push_losses.append(push_loss)
+                pull_losses.append(pull_loss)
+            else:
+                push_losses.append(None)
+                pull_losses.append(None)
+
+        return heatmaps_losses, push_losses, pull_losses
